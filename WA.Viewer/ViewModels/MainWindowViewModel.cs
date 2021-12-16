87417db0b818e7ebde433f3f64648a5e8ce361e3
longod <forferelden@gmail.com>
2021-12-16 20:47:35 +0900

    add missing updates

--- WA.Viewer/ViewModels/MainWindowViewModel.cs
+++ WA.Viewer/ViewModels/MainWindowViewModel.cs
@@ -31,23 +31,32 @@ namespace WA.Viewer.ViewModels
         public DelegateCommand ShowSettingsWindowCommand { get; }
         public DelegateCommand ExitCommand { get; }
 
+        public DelegateCommand<RoutedEventArgs> LoadedCommand { get; }
+        public DelegateCommand<DragEventArgs> PreviewDragOverCommand { get; }
+        public DelegateCommand<DragEventArgs> DropCommand { get; }
+
         // test
         public DelegateCommand ShowConfigCommand { get; }
         PluginManager _pluginManager;
 
-        public MainWindowViewModel(IRegionManager regionManager, IDialogService dialogService, ViewerModel viewer, PluginManager pluginManager)
+        public MainWindowViewModel(IRegionManager regionManager, IDialogService dialogService, AppSettings settings, ViewerModel viewer, PluginManager pluginManager)
         {
+            _viewer = viewer;
+            Task.Run(() => _viewer.ProcessAsync());
+
             _regionManager = regionManager;
             _dialogService = dialogService;
-            _viewer = viewer;
+            _pluginManager = pluginManager; // test
+
             Image = _viewer.ObserveProperty(x => x.Image).ToReadOnlyReactivePropertySlim().AddTo(_disposable);
-            Task.Run(() => _viewer.ProcessAsync());
 
             ExitCommand = new DelegateCommand(Exit);
             ShowSettingsWindowCommand = new DelegateCommand(ShowSettingsWindow);
             ShowConfigCommand = new DelegateCommand(ShowConfigTest);
 
-            _pluginManager = pluginManager;
+            LoadedCommand = new DelegateCommand<RoutedEventArgs>(Loaded);
+            PreviewDragOverCommand = new DelegateCommand<DragEventArgs>(ImagePreviewDragOver);
+            DropCommand = new DelegateCommand<DragEventArgs>(ImageDrop);
         }
 
 
@@ -61,6 +70,42 @@ namespace WA.Viewer.ViewModels
                     region.RemoveAll();
                 }
             }
+        }
+
+        private void Loaded(RoutedEventArgs e)
+        {
+#if true // todo only development
+            var args = Environment.GetCommandLineArgs();
+            // include self dll name
+            if (args.Length >= 2)
+            {
+                if (args[1] == "-k")
+                {
+                    System.Diagnostics.Process.GetCurrentProcess().Kill(); // force kill
+                }
+                else if (args[1] == "-e")
+                {
+                    Application.Current.Shutdown();
+                }
+            }
+#endif
+        }
+
+        private void ImagePreviewDragOver(DragEventArgs e)
+        {
+            // マウスカーソルをコピーにする。
+            e.Effects = DragDropEffects.Copy;
+            // ドラッグされてきたものがFileDrop形式の場合だけ、このイベントを処理済みにする。
+            e.Handled = e.Data.GetDataPresent(DataFormats.FileDrop);
+        }
+
+        // ImageのDropイベントに対する処理
+        private void ImageDrop(DragEventArgs e)
+        {
+            // ドロップされたものがFileDrop形式の場合は、各ファイルのパス文字列を文字列配列に格納する。
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            // 複数ドロップの可能性もあるので、今回は最初のファイルを選択して表示
+            //ViewImage.Value = files[0];
         }
 
         private void Exit()
