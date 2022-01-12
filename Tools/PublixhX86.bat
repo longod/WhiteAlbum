cd2d7fe652d75a6b954ec380043cd7c13dac720c
longod <forferelden@gmail.com>
2022-01-12 10:33:36 +0900

    update publish option

--- Tools/PublixhX86.bat
+++ Tools/PublixhX86.bat
@@ -1,8 +1,8 @@
 @echo off
 pushd %~dp0\..\
 
-@rem todo use p:PublishSingleFile=true
-@rem https://www.hanselman.com/blog/making-a-tiny-net-core-30-entirely-selfcontained-single-executable
-dotnet publish -p:Configuration=Release -p:Platform=x86 -r win-x86 --self-contained false -o Publish\
+@rem 共有ファイルも一つになるのでexeの数分容量が増えがち
+@rem dotnet publish -p:Configuration=Release -p:Platform=x86 -r win-x86 --self-contained false -p:DebugType=None -p:PublishSingleFile=true -o Publish\
+dotnet publish -p:Configuration=Release -p:Platform=x86 -r win-x86 --self-contained false -p:DebugType=None -o Publish\
 
 popd
